f1233916e430c0df07f34d973295698e718f4d55
Hisham-Echo <hishamsaleh6090@gmail.com>
2026-04-05 16:23:41 +0200

    first commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://tasleem-frontend.vercel.app',],
+    'allowed_origins' => ['*', 'https://tasleem-frontend.vercel.app',],
 
     'allowed_origins_patterns' => [],
 
